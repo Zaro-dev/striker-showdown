7815b699726a55df7262fb9672f27a90e676b27d
Mario <mariolazaroredolar@gmail.com>
2024-07-01 21:47:34 +0200

    creación archivos/primeras configuraciones/gameLoop

---
+++ JS/main.js
@@ -0,0 +1,31 @@
+//* ELEMENTOS PRINCIPALES DEL DOM
+
+// pantallas
+const splashScreenNode = document.querySelector("#splash-screen");
+const gameScreenNode = document.querySelector("#game-screen");
+const finalScreenNode = document.querySelector("#winner-screen");
+
+// botones
+const startBtnNode = document.querySelector("start-btn");
+
+// game box
+
+
+
+//* VARIABLES GLOBALES DEL JUEGO
+
+
+
+//* FUNCIONES GLOBALES DEL JUEGO
+function startGame(){
+    splashScreenNode.style.display = "none";
+
+    gameScreenNode.style.display = "flex";
+
+    mainIntervalId = setInterval(() => {
+        gameLoop();
+    }, Math.round(1000/60));
+}
+
+//* EVENT LISTENERS
+
